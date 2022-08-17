557551fe2a73310de0618100f5bee0dfb14a2134
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-17 17:53:13 +0200

    Eliminmos Ribbon de la app porque ya se maneja en Eureka

--- src/main/java/com/jmunoz/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/jmunoz/springboot/app/item/SpringbootServicioItemApplication.java
@@ -3,14 +3,14 @@ package com.jmunoz.springboot.app.item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // Con solo informar la dependencia en el pom (eureka-client) de forma automática se configura como cliente.
 // Pero igualmente es buena práctica (no obligatorio) indicarlo con una anotación en la clase principal.
 // Se indica de forma explícita con la anotación @EnableEurekaClient
+//
+// Ribbon no es necesario pero Feign si, ya que lo necesitamos para conectarnos a las API REST
 @EnableEurekaClient
-@RibbonClient(name="servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
