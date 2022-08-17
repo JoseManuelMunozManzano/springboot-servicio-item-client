4b6fd15081621536d17ad67ce43d0b3c6f316f2d
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-17 17:40:04 +0200

    Se anota con @EnableEurekaClient

--- src/main/java/com/jmunoz/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/jmunoz/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,22 +2,14 @@ package com.jmunoz.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-// Se va a ver la librería Feign, otra forma de implementar un cliente http para la comunicación entre microservicios.
-// Es una alternativa a utilizar RestTemplate. Es más sencilla, usando interfaces y anotaciones de forma declarativa.
-//
-// Feign es desarrollada por Netflix y forma parte de Spring Cloud.
-// Se integra con el balanceo de carga, de Ribbon y también viene configurado en los clientes de Eureka...
-// Por ahora lo vamos a hacer sencillo, centrándonos en el uso de Feign como cliente http.
-//
-// Con la anotación @EnableFeignClients, se habilitan nuestros clientes Feign que tengamos implementado en el
-// proyecto, y además nos permite inyectar estos clientes en nuestros controladores u otros componentes de Spring,
-// es decir, habilita la inyección de dependencias, el @Autowired
-//
-// Configuración de Ribbon Client para que funcione la parte del properties
-// En la anotación @RibbonClient se indica el mismo nombre que tenemos en ProductoClienteRest
+// Con solo informar la dependencia en el pom (eureka-client) de forma automática se configura como cliente.
+// Pero igualmente es buena práctica (no obligatorio) indicarlo con una anotación en la clase principal.
+// Se indica de forma explícita con la anotación @EnableEurekaClient
+@EnableEurekaClient
 @RibbonClient(name="servicio-productos")
 @EnableFeignClients
 @SpringBootApplication
