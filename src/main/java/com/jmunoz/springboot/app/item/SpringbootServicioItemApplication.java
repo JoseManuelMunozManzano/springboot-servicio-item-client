89b711b9626e8b6d462dcc694a3b1d1927a00b5c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-20 05:22:35 +0200

    Añadiendo Hystrix

--- src/main/java/com/jmunoz/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/jmunoz/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,6 +2,7 @@ package com.jmunoz.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -10,6 +11,17 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 // Se indica de forma explícita con la anotación @EnableEurekaClient
 //
 // Ribbon no es necesario pero Feign si, ya que lo necesitamos para conectarnos a las API REST
+//
+// Habilitamos Hystrix con la anotación @EnableCircuitBreaker. Se encarga, mediante un hilo separado, de la
+// comunicación de los microservicios. Envuelve a Ribbon, que aunque no lo tenemos en el POM viene incluido por
+// defecto en el cliente Eureka (hasta la versión de SpringBoot 2.3), para tolerancia a fallos, manejo de latencia
+// y time-out.
+// Para probar Hystrix, en el proyecto de servicio de productos
+// (https://github.com/JoseManuelMunozManzano/springboot-servicio-productos-client) se va a simular un error
+//
+// A partir de la versión de SpringBoot 2.4 en vez de Ribbon viene Spring Cloud Load Balancer y en vez de Hystrix
+// hay que usar Resilience4j.
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
