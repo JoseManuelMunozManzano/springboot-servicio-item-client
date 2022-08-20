6db908a245709b1c3c978f3730125bf8879ad65a
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-20 05:35:27 +0200

    Hystrix. Cortocircuito y camino alternativo

--- src/main/java/com/jmunoz/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/jmunoz/springboot/app/item/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package com.jmunoz.springboot.app.item.controllers;
 
 import com.jmunoz.springboot.app.item.models.Item;
+import com.jmunoz.springboot.app.item.models.Producto;
 import com.jmunoz.springboot.app.item.models.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +23,31 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    // Se ha provocado un error en el servicio producto.
+    // https://github.com/JoseManuelMunozManzano/springboot-servicio-productos-client
+    // Ver commit: Generado error para probar Hystrix
+    //
+    // En caso de fallo se indica un método alternativo.
+    // Es lo que se llama un cortocircuito. Cerramos el camino actual y derivamos a otro camino alternativo.
+    // Evitamos errores en cascada.
+    @HystrixCommand(fallbackMethod = "metodoAlternativo")
     @GetMapping("/ver/{id}/cantidad/{cantidad}")
     public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
         return itemService.findById(id, cantidad);
+    }
+
+    // El método alternativo tiene que tener la misma cabecera que detalle. Cambia la implementación.
+    public Item metodoAlternativo(@PathVariable Long id, @PathVariable Integer cantidad) {
+        Item item = new Item();
+        Producto producto = new Producto();
+
+        // Esto podría hacer uso de otro endpoint, usando Feign...
+        // Aquí se pasa un item hecho a mano
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Camara Sony");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return item;
     }
 }
